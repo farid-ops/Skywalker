4ed77e5efa952126c920f41d140afcabb4246662
farid chaibou <farid-ops>
2022-05-14 18:45:56 +0100

    init

---
+++ src/main/java/monckey/chopper/SkypeaApplication.java
@@ -0,0 +1,13 @@
+package monckey.chopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkypeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/monckey/chopper/SkypeaApplicationTests.java
@@ -0,0 +1,13 @@
+package monckey.chopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkypeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
