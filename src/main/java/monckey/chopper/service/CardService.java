a93bb2ceb4a2b18f3c4a1e78e9544e5adfcce919
farid-ops <55166975+farid-ops@users.noreply.github.com>
2022-06-11 16:21:12 +0100

    define card service methods

--- src/main/java/monckey/chopper/service/CardService.java
+++ src/main/java/monckey/chopper/service/CardService.java
@@ -1,2 +1,17 @@
-package monckey.chopper.service;public interface CardService {
+package monckey.chopper.service;
+
+import monckey.chopper.entity.Card;
+
+import java.util.Iterator;
+import java.util.Optional;
+
+public interface CardService {
+
+    Iterator<Card> getAllCards();
+
+    Optional<Card> getOneCard(String cardId);
+
+    Optional<Card> saveCard(Card card);
+
+    void deleteCard(String cardId);
 }
