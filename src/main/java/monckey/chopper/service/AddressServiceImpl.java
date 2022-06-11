c049cb28e49607230368e8b9d41f7e5f86dc87df
farid-ops <55166975+farid-ops@users.noreply.github.com>
2022-06-11 16:20:36 +0100

    Implement address method

--- src/main/java/monckey/chopper/service/AddressServiceImpl.java
+++ src/main/java/monckey/chopper/service/AddressServiceImpl.java
@@ -1,2 +1,40 @@
-package monckey.chopper.service;public class AddressServiceImpl {
+package monckey.chopper.service;
+
+import monckey.chopper.entity.Address;
+import monckey.chopper.error.ResourceNotFoundException;
+import monckey.chopper.repo.AddressRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressRepository address;
+
+    public AddressServiceImpl(AddressRepository address){
+        this.address = address;
+    }
+
+    @Override
+    public List<Address> getAllAddress() {
+        return address.findAll();
+    }
+
+    @Override
+    public Optional<Address> getOneAddress(String addressId) {
+       return this.address.findById(UUID.fromString(addressId));
+    }
+
+    @Override
+    public Address saveAddress(Address address) {
+        return this.address.save(address);
+    }
+
+    @Override
+    public void deleteAddresse(String addressId) {
+        this.address.deleteById(UUID.fromString(addressId));
+    }
 }
